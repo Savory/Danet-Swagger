5a775793f27f821db1c219dbbc3d4e39b7757885
Sorikairo <sorikairox@gmail.com>
2023-04-08 15:13:58 +0900

    refactor: inline swagger html

--- mod.ts
+++ mod.ts
@@ -81,11 +81,21 @@ export class SwaggerModule {
 		app: DanetApplication,
 		document: Swagger.Spec,
 	) {
-		const url = new URL('./swagger.html', import.meta.url).href;
-		const swaggerHtml = await (await fetch(url)).text();
+		// const url = new URL('./swagger.html', import.meta.url).href;
+		// const swaggerHtml = await (await fetch(url)).text();
 		apiPath = trimSlash(apiPath);
 		app.danetRouter.router.get(`/${apiPath}`, async (context, next) => {
-			context.response.body = swaggerHtml;
+			context.response.body = `<!DOCTYPE html><html lang="en">
+<head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="description" content="SwaggerUI"/><title>SwaggerUI</title>
+<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" /></head>
+<body><div id="swagger-ui"></div>
+<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js" crossorigin></script>
+<script>window.onload = () => {
+let url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname.split("/").filter(s => s.length > 0).join("/");
+window.ui = SwaggerUIBundle({url: url + "/json",dom_id: "#swagger-ui", presets: [SwaggerUIBundle.presets.apis,SwaggerUIStandalonePreset],layout: "StandaloneLayout"});};
+</script></body></html>`;
 			await next();
 		});
 		app.danetRouter.router.get(`/${apiPath}/json`, async (context, next) => {
